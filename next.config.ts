1e12b6fb2f65d8a9aa8b537845906ef86d19853f
zandimt <andimtoader@gmail.com>
2025-03-14 14:23:14 +0100

    modified configs AGAIN

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
     reactStrictMode: true,
     images: {
         unoptimized: true, // Disable default image optimization
     },
-    assetPrefix: isProd ? '/your-repository-name/' : '',
-    basePath: isProd ? '/your-repository-name' : '',
-    output: 'export'
+    output: 'export',
 };
 
 export default nextConfig;
