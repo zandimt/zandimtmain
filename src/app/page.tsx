95250c3604030070dd36313dbb5aff3861163113
zandimt <andimtoader@gmail.com>
2025-06-28 17:56:59 +0200

    no more blue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <p className="text-lg text-gray-600 dark:text-gray-300 mb-8">Welcome to zandi&apos;s personal portofolio and blog.</p>
       <Link
         href="/blog"
-        className="text-blue-600 dark:text-blue-400 underline hover:text-blue-800 dark:hover:text-blue-200 transition"
+        className="text-blue-600 dark:text-gray-200 underline hover:text-gray-600 dark:hover:text-gray-400 transition"
       >
         blog
       </Link>
